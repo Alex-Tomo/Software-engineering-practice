321381cde6fc0901150fed14537ae9feb813ade7
Stovesy25 <61972744+Stovesy25@users.noreply.github.com>
2021-04-19 19:07:56 +0100

    Added timer to review section

--- Software-engineering-practice/public/js/reviews.js
+++ Software-engineering-practice/public/js/reviews.js
@@ -5,6 +5,7 @@ let prevSlideAnchor = document.getElementsByClassName('prev');
 window.onload = function() {
     let slideIndex = 1;
     showSlides(slideIndex);
+    carousel();
 
     // Added by alex, removed the home.php onclicks, use these instead (more dynamic).
     nextSlideAnchor[0].addEventListener("click", function() { plusSlides(1); });
@@ -28,5 +29,23 @@ window.onload = function() {
         }
         slides[slideIndex-1].style.display = "block";
         dots[slideIndex-1].className += " active";
+    }
+
+    function carousel() {
+        let i;
+        let x = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        slideIndex++;
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        if (slideIndex > x.length) {slideIndex = 1}
+        x[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+
+        setTimeout(carousel, 7000);
     }
 }
