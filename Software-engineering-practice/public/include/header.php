b1776fe1d5bb468382d3d3b47fb61de09e617884
Stovesy25 <61972744+Stovesy25@users.noreply.github.com>
2021-04-20 21:27:48 +0100

    Added name option on desktop menu

--- Software-engineering-practice/public/include/header.php
+++ Software-engineering-practice/public/include/header.php
@@ -10,12 +10,21 @@ function getHeader() {
             <h1 class='clickable' onclick='openPage(`loggedinHome.php`)'>skip</h1><h1 class='clickable' id='logoText2' onclick='openPage('home.php')'>CV</h1>
             <nav>";
                 if (!$_SESSION['loggedin']) {
-                    $header .= "<button class='clickable' onclick = 'openPage(`register.php`)' id = 'signUp' > Sign Up </button >
-                    <button class='clickable' onclick = 'openPage(`signin.php`)' id = 'login' ><a class='links' > Login</a ></button>";
+                    $header .= "<button class='clickable' onclick='openPage(`register.php`)' id='signUp'>Sign Up</button >
+                    <button class='clickable' onclick='openPage(`signin.php`)' id ='login'><a class='links'>Login</a ></button>";
                 } else {
-                    $header .= "<a id='topLink' class='clickable' onclick='openPage(`loggedinHome.php`)'>Home</a>
-                    <button class='clickable' onclick='openPage(`logout.php`)' id='login'>Logout</button>";
-                }
+                    $header .= "
+                                  <div class='dropdown'>
+                                    <button class='dropbtn'>Hi, John
+                                      <i class='fa fa-caret-down'></i>
+                                    </button>
+                                    <img src='assets/photo.svg'>
+                                    <div class='dropdown-content'>
+                                        <a class='links clickable' onclick='openPage(`loggedinHome.php`)'>Home</a>
+                                        <a class='links clickable' onclick='openPage(`postJob.php`)'>Post a Job</a>
+                                        <a class='links clickable' onclick='openPage(`logout.php`)'>Logout</a>
+                                    </div>
+                    ";}
                 $header .= "</nav>
         </div>";
 
@@ -25,11 +34,12 @@ function getHeader() {
                 <h1 class='clickable' onclick='openPage(`home.php`)'>skip</h1><h1 class='clickable' id='logoText2' onclick='openPage('home.php')'>CV</h1>
                 <div id='myLinks'>";
                     if (!$_SESSION['loggedin']) {
-                        $header .= "<button class='clickable' onclick = 'openPage(`register.php`)' id = 'signUpMob' > Sign Up </button ><br >
-                        <button class='clickable' onclick = 'openPage(`signin.php`)' id = 'loginMob' > Login</button>";
+                        $header .= "<button class='clickable' onclick='openPage(`register.php`)' id='signUpMob'>Sign Up</button ><br >
+                        <button class='clickable' onclick='openPage(`signin.php`)' id='loginMob'>Login</button>";
                     } else {
-                        $header .= "<a id='topLink' class='clickable' onclick='openPage(`loggedinHome.php`)'>Home</a>
-                        <button class='clickable' onclick='openPage(`logout.php`)' id='login'>Logout</button>";
+                        $header .= "<a id='mobHome' class='clickable' onclick='openPage(`loggedinHome.php`)'>Home</a>
+                        <a id='jobLink' class='clickable' onclick='openPage(`postJob.php`)'>Post a Job</a>
+                        <button class='clickable' onclick='openPage(`logout.php`)' id='logout'>Logout</button>";
                     }
                 $header .= "</div>
                 <a href='javascript:void(0);' class='icon' onclick='hambgrMenu()'>
