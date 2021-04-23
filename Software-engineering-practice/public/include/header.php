5d1b0693008457bd0804bf698ff7620606742cd7
Stovesy25 <61972744+Stovesy25@users.noreply.github.com>
2021-04-23 20:36:59 +0100

    Added different colour user icon

--- Software-engineering-practice/public/include/header.php
+++ Software-engineering-practice/public/include/header.php
@@ -8,7 +8,7 @@ function getHeader() {
     $arr = explode("/", __DIR__);
     foreach ($arr as $a) {
         $path .= $a.'/';
-        if ($a == 'Software-engineering-practice') {
+        if ($a == 'sep') {
             $path .= 'db_connector.php';
             break;
         }
@@ -16,31 +16,30 @@ function getHeader() {
 
     include_once $path;
 
-
     $header = "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
     
     <header>";
-        // Desktop Navbar
-        $header .= "<div id='desktop_container'>
+    // Desktop Navbar
+    $header .= "<div id='desktop_container'>
             <h1 class='clickable' onclick='openPage(`loggedinHome.php`)'>skip</h1><h1 class='clickable' id='logoText2' onclick='openPage(`loggedinHome.php`)'>CV</h1>
             <nav>";
-                if (!$_SESSION['loggedin']) {
-                    $header .= "<button class='clickable' onclick='openPage(`register.php`)' id='signUp'>Sign Up</button >
+    if (!$_SESSION['loggedin']) {
+        $header .= "<button class='clickable' onclick='openPage(`register.php`)' id='signUp'>Sign Up</button >
                     <button class='clickable' onclick='openPage(`signin.php`)' id ='login'><a class='links'>Login</a ></button>";
-                } else {
-                    $header .= "<div id='dropdown'>
-                                    <button id='dropbtn' class='clickable' onclick='userDrpdwn()'>";
-                    $conn = getConnection();
-                    $sqlQuery = $conn->query("SELECT user_fname FROM sep_user_info 
-                                                       INNER JOIN sep_users ON sep_user_info.user_id = sep_users.user_id 
-                                                       WHERE user_email = '".$_SESSION['email']."'");
-                    if($sqlQuery){
-                        while($name = $sqlQuery->fetchObject()) {
-                            $header .= "<div>Hi, {$name->user_fname} <i class='fa fa-caret-down'></i></div>";
-                            }
-                    }
-                    $header .= "</button>
-                                    <img src='assets/person.svg'>
+    } else {
+        $header .= "<div id='dropdown'>
+                    <button id='dropbtn' class='clickable' onclick='userDrpdwn()'>";
+        $conn = getConnection();
+        $sqlQuery = $conn->query("SELECT user_fname FROM sep_user_info 
+                                  INNER JOIN sep_users ON sep_user_info.user_id = sep_users.user_id 
+                                  WHERE user_email = '".$_SESSION['email']."'");
+        if($sqlQuery){
+            while($name = $sqlQuery->fetchObject()) {
+                $header .= "<div>Hi, {$name->user_fname} <i class='fa fa-caret-down'></i></div>";
+            }
+        }
+        $header .= "</button>
+                                    <img src='assets/navPerson.svg'>
                                     <div id='dropdown_content'>
                                         <a class='links clickable' onclick='openPage(`userProfile.php`)'>My Profile</a>
                                         <a class='links clickable' onclick='openPage(`userJobs.php`)'>My Jobs</a>
@@ -54,23 +53,36 @@ function getHeader() {
                                     <li><a class='links clickable' onclick='openPage(`loggedinHome.php`)'>Home</a></li>
                     </ul>
                                     ";}
-                $header .= "</nav>
+    $header .= "</nav>
         </div>";
 
-        // Mobile NavBar
-        $header .= "<div id='mobile_container'>
+    // Mobile NavBar
+    $header .= "<div id='mobile_container'>
             <div id='topnav'>
                 <h1 class='clickable' onclick='openPage(`home.php`)'>skip</h1><h1 class='clickable' id='logoText2' onclick='openPage(`home.php`)'>CV</h1>
                 <div id='myLinks'>";
-                    if (!$_SESSION['loggedin']) {
-                        $header .= "<button class='clickable' onclick='openPage(`register.php`)' id='signUpMob'>Sign Up</button ><br >
+    if (!$_SESSION['loggedin']) {
+        $header .= "<button class='clickable' onclick='openPage(`register.php`)' id='signUpMob'>Sign Up</button ><br >
                         <button class='clickable' onclick='openPage(`signin.php`)' id='loginMob'>Login</button>";
-                    } else {
-                        $header .= "<a id='mobHome' class='clickable' onclick='openPage(`loggedinHome.php`)'>Home</a>
-                        <a id='jobLink' class='clickable' onclick='openPage(`postJob.php`)'>Post a Job</a>
-                        <button class='clickable' onclick='openPage(`logout.php`)' id='logout'>Logout</button>";
-                    }
-                $header .= "</div>
+    } else {
+        $conn = getConnection();
+        $sqlQuery = $conn->query("SELECT user_fname FROM sep_user_info 
+                                                       INNER JOIN sep_users ON sep_user_info.user_id = sep_users.user_id 
+                                                       WHERE user_email = '".$_SESSION['email']."'");
+        if($sqlQuery){
+            while($name = $sqlQuery->fetchObject()) {
+                $header .= "<div class='clickable' onclick='openPage(`userProfile.php`)' id='mobName'>Hi, {$name->user_fname}</div>";
+            }
+        }
+        $header .= "<a class='mobLinks clickable' onclick='openPage(`loggedinHome.php`)'>Home</a>
+                                    <a class='mobLinks clickable' onclick='openPage(`userProfile.php`)'>My Profile</a>
+                                    <a class='mobLinks clickable' onclick='openPage(`userJobs.php`)'>My Jobs</a>
+                                    <a class='mobLinks clickable' onclick='openPage(`postJob.php`)'>Post a Job</a>
+                                    <a class='mobLinks clickable' onclick='openPage(`postJob.php`)'>Messages</a>
+                                    <a class='mobLinks clickable' onclick='openPage(`postJob.php`)'>Testimonials</a>
+                                    <button class='clickable' onclick='openPage(`logout.php`)' id='logout'>Logout</button>";
+    }
+    $header .= "</div>
                 <a href='javascript:void(0);' class='icon' onclick='hambgrMenu()'>
                     <i class='fa fa-bars'></i>
                 </a>
