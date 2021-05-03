d4f551a69404bf4d66b5d1dc71f491505993b386
Stovesy25 <61972744+Stovesy25@users.noreply.github.com>
2021-05-03 18:59:28 +0100

    Add files via upload

--- Software-engineering-practice/public/js/formErrors.js
+++ Software-engineering-practice/public/js/formErrors.js
@@ -2,7 +2,7 @@ const error = document.getElementById('error');
 
 document.getElementById('password').addEventListener('input', (e) => {
     if ('password' < 0){
-        error.textContent = 'A filename cannot contain any of the following characters: \/:*?"<>|';
+        error.textContent = 'Password must be more characters';
     } else {
         error.textContent = '';
     }
